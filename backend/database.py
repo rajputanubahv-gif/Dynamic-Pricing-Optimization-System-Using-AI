cdfe24ced2e85f58047b30a034d7943329fdc4c5
Anubhav Rajput <rajanubhav@gmail.com>
2026-03-26 09:56:44 +0530

    Removed frontend folder from main branch

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/dynamic_pricing"
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(bind=engine)
